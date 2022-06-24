e788adde10a3384fe435bd8ae0cfcf680bc223b7
EluciusFTW <guysbuss@gmail.com>
2022-06-24 15:03:23 +0200

    Added settings validation example

--- commands/Hello.fs
+++ commands/Hello.fs
@@ -4,11 +4,16 @@ module Greet =
     open Spectre.Console.Cli
     open Output
 
-    type HelloSettings() =
+    type HelloSettings() as self =
         inherit CommandSettings()
 
         [<CommandOption("-n|--name")>]
         member val name = "friend" with get, set
+
+        override _.Validate() =
+            match self.name.Length with
+            | 1 -> Spectre.Console.ValidationResult.Error($"That's an awfully short name, I don't buy it.")
+            | _ -> Spectre.Console.ValidationResult.Success()
     
     type Hello() =
         inherit Command<HelloSettings>()
