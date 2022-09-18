ec87bcbcbbaadced5e24605054a7057755dfb268
EluciusFTW <guysbuss@gmail.com>
2022-09-18 10:12:01 +0200

    Changed module to namespace

--- spectre/Output.fs
+++ spectre/Output.fs
@@ -1,11 +1,11 @@
 module Output
-    open Spectre.Console
+open Spectre.Console
 
-    let primaryStyle = "green"
-    let warningStyle = "red"
+let primaryStyle = "green"
+let warningStyle = "red"
 
-    let markup style content = $"[{style}]{content}[/]"
-    let emphasize content = markup primaryStyle content
-    let warn content = markup warningStyle content
+let markup style content = $"[{style}]{content}[/]"
+let emphasize content = markup primaryStyle content
+let warn content = markup warningStyle content
 
-    let printMarkedUp content = AnsiConsole.Markup $"{content}{System.Environment.NewLine}"
+let printMarkedUp content = AnsiConsole.Markup $"{content}{System.Environment.NewLine}"
