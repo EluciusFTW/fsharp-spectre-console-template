bddc2464f246855c7e2fb8b171b679a6fe5d2817
EluciusFTW <guysbuss@gmail.com>
2023-07-30 14:23:32 +0200

    Upgrade to .NET7 and Spectre 0.47

--- spectre/Output.fs
+++ spectre/Output.fs
@@ -2,10 +2,10 @@ module Output
 open Spectre.Console
 
 let primaryStyle = "green"
-let warningStyle = "red"
 
-let markup style content = $"[{style}]{content}[/]"
-let emphasize content = markup primaryStyle content
-let warn content = markup warningStyle content
+let markup style content = 
+    $"[{style}]{content}[/]"
+let emphasize content = 
+    markup primaryStyle content
 
 let printMarkedUp content = AnsiConsole.Markup $"{content}{System.Environment.NewLine}"
